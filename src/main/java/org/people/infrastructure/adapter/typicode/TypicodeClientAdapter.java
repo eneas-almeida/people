dc2e96b5848382af0bce11c548f9ef5d52d3f031
eneas <eneas.eng@yahoo.com>
2025-12-16 17:54:51 -0300

    feat: ajustes de properties

--- src/main/java/org/people/infrastructure/adapter/typicode/TypicodeClientAdapter.java
+++ src/main/java/org/people/infrastructure/adapter/typicode/TypicodeClientAdapter.java
@@ -2,30 +2,42 @@ package org.people.infrastructure.adapter.typicode;
 
 import org.people.domain.client.UserClient;
 import org.people.domain.entity.User;
-import org.people.infrastructure.properties.TypicodeClientProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
 public class TypicodeClientAdapter implements UserClient {
-	private final WebClient webClient;
+
 	private final UserMapper userMapper;
 
+	private final WebClient.Builder webClient;
+
 	public TypicodeClientAdapter(
-			WebClient.Builder webClientBuilder,
 			UserMapper userMapper,
-			TypicodeClientProperties properties) {
-		this.webClient = webClientBuilder
-				.baseUrl(properties.getBaseUrl())
-				.build();
+			WebClient.Builder webClient) {
 		this.userMapper = userMapper;
+		this.webClient = webClient;
 	}
+
+	@Value("${client.typicode.base-url}")
+	private String baseUrl;
 
 	@Override
 	public Mono<User> findById(Integer id) {
+		final ExchangeStrategies strategies = ExchangeStrategies.builder()
+				.codecs(config -> config
+						.defaultCodecs()
+						.maxInMemorySize(16 * 1024 * 1024))
+				.build();
+
 		return webClient
+				.baseUrl(baseUrl)
+				.exchangeStrategies(strategies)
+				.build()
 				.get()
 				.uri("/users/{id}", id)
 				.retrieve()
@@ -35,15 +47,21 @@ public class TypicodeClientAdapter implements UserClient {
 
 	@Override
 	public Flux<User> listAll() {
-		try {
-			return webClient
-					.get()
-					.uri("/users")
-					.retrieve()
-					.bodyToFlux(UserResponse.class)
-					.map(userMapper::toUser);
-		} catch (Exception e) {
-			return Flux.error(new RuntimeException("Failed to fetch users", e));
-		}
+		final ExchangeStrategies strategies = ExchangeStrategies.builder()
+				.codecs(config -> config
+						.defaultCodecs()
+						.maxInMemorySize(16 * 1024 * 1024))
+				.build();
+
+
+		return webClient
+				.baseUrl(baseUrl)
+				.exchangeStrategies(strategies)
+				.build()
+				.get()
+				.uri("/users")
+				.retrieve()
+				.bodyToFlux(UserResponse.class)
+				.map(userMapper::toUser);
 	}
 }
