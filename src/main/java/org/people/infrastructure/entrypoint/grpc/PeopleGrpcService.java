775d2b7dce1970bbfa4a722f00aeca29e53cecc2
eneas <eneas.eng@yahoo.com>
2025-12-22 15:31:11 -0300

    feat: atualiza o projeto, adicionando o grpc reactor

--- src/main/java/org/people/infrastructure/entrypoint/grpc/PeopleGrpcService.java
+++ src/main/java/org/people/infrastructure/entrypoint/grpc/PeopleGrpcService.java
@@ -1,15 +1,17 @@
 package org.people.infrastructure.entrypoint.grpc;
 
-import com.people.grpc.ServiceProto;
-import com.people.grpc.PeopleServiceGrpc;
-import io.grpc.Status;
-import io.grpc.stub.StreamObserver;
+import com.people.grpc.ReactorPeopleServiceGrpc;
+import com.people.grpc.ServiceProto.ListPeopleRequestGrpc;
+import com.people.grpc.ServiceProto.ListPeopleResponseGrpc;
+import com.people.grpc.ServiceProto.PeopleRequestGrpc;
+import com.people.grpc.ServiceProto.PeopleResponseGrpc;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.people.usecase.GetPeopleUseCase;
 import org.people.usecase.ListPeopleUseCase;
+import reactor.core.publisher.Mono;
 
 @GrpcService
-public class PeopleGrpcService extends PeopleServiceGrpc.PeopleServiceImplBase {
+public class PeopleGrpcService extends ReactorPeopleServiceGrpc.PeopleServiceImplBase {
 	private final GetPeopleUseCase getPeopleUseCase;
 	private final ListPeopleUseCase listPeopleUseCase;
 
@@ -19,54 +21,28 @@ public class PeopleGrpcService extends PeopleServiceGrpc.PeopleServiceImplBase {
 	}
 
 	@Override
-	public void getPeople(ServiceProto.PeopleRequest request, StreamObserver<ServiceProto.PeopleResponse> responseObserver) {
-		int peopleId = request.getId();
-
-		getPeopleUseCase.execute(peopleId)
-				.subscribe(
-						people -> {
-							ServiceProto.PeopleResponse response = ServiceProto.PeopleResponse.newBuilder()
-									.setId(people.getId())
-									.setName(people.getName())
-									.setEmail(people.getEmail())
-									.build();
-
-							responseObserver.onNext(response);
-							responseObserver.onCompleted();
-						},
-						error -> {
-							responseObserver.onError(Status.INTERNAL
-									.withDescription("Error fetching people: " + error.getMessage())
-									.asRuntimeException());
-						}
-				);
+	public Mono<PeopleResponseGrpc> getPeople(Mono<PeopleRequestGrpc> request) {
+		return request
+				.flatMap(req -> getPeopleUseCase.execute(req.getId()))
+				.map(people -> PeopleResponseGrpc.newBuilder()
+						.setId(people.getId())
+						.setName(people.getName())
+						.setEmail(people.getEmail())
+						.build());
 	}
 
 	@Override
-	public void listPeople(ServiceProto.ListPeopleRequest request, StreamObserver<ServiceProto.ListPeopleResponse> responseObserver) {
-		listPeopleUseCase.execute()
+	public Mono<ListPeopleResponseGrpc> listPeople(Mono<ListPeopleRequestGrpc> request) {
+		return request
+				.flatMapMany(req -> listPeopleUseCase.execute())
+				.map(people -> PeopleResponseGrpc.newBuilder()
+						.setId(people.getId())
+						.setName(people.getName())
+						.setEmail(people.getEmail())
+						.build())
 				.collectList()
-				.subscribe(
-						peopleList -> {
-							ServiceProto.ListPeopleResponse.Builder responseBuilder = ServiceProto.ListPeopleResponse.newBuilder();
-
-							peopleList.forEach(people -> {
-								ServiceProto.PeopleResponse peopleResponse = ServiceProto.PeopleResponse.newBuilder()
-										.setId(people.getId())
-										.setName(people.getName())
-										.setEmail(people.getEmail())
-										.build();
-								responseBuilder.addPeople(peopleResponse);
-							});
-
-							responseObserver.onNext(responseBuilder.build());
-							responseObserver.onCompleted();
-						},
-						error -> {
-							responseObserver.onError(Status.INTERNAL
-									.withDescription("Error listing people: " + error.getMessage())
-									.asRuntimeException());
-						}
-				);
+				.map(peopleList -> ListPeopleResponseGrpc.newBuilder()
+						.addAllPeople(peopleList)
+						.build());
 	}
 }
